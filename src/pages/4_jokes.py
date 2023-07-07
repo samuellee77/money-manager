22546b14ea4054bc489c4750fa12292a429f016e
samuellee77 <hsl023@ucsd.edu>
2023-07-07 15:03:38 +0800

    Update 4_jokes.py

--- src/pages/4_jokes.py
+++ src/pages/4_jokes.py
@@ -5,14 +5,13 @@ import os
 
 def main():
     st.title("爛笑話")
-    # if st.button("生成"):
-    st.write(os.path.exists("/data"))
-        # with open("data/joke.json", encoding='utf-8') as f:
-        #     data = json.load(f)
-        #     title, content = random.choice(list(data.items()))
-        #     st.subheader(title)
-        #     for i in range(len(content)):
-        #         st.write(content[i])
+    if st.button("生成"):
+        with open("src/data/joke.json", encoding='utf-8') as f:
+            data = json.load(f)
+            title, content = random.choice(list(data.items()))
+            st.subheader(title)
+            for i in range(len(content)):
+                st.write(content[i])
 
 if __name__ == '__main__':
     main()
