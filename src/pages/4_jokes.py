ea447bbae3fb62ae8214c5f56a735c1860127f77
samuellee77 <hsl023@ucsd.edu>
2023-07-07 14:59:50 +0800

    Update 4_jokes.py

--- src/pages/4_jokes.py
+++ src/pages/4_jokes.py
@@ -1,16 +1,18 @@
 import json
 import streamlit as st
 import random
+import os 
 
 def main():
     st.title("爛笑話")
     if st.button("生成"):
-        with open("data/joke.json", encoding='utf-8') as f:
-            data = json.load(f)
-            title, content = random.choice(list(data.items()))
-            st.subheader(title)
-            for i in range(len(content)):
-                st.write(content[i])
+        st.write(os.path.exists("/data"))
+        # with open("data/joke.json", encoding='utf-8') as f:
+        #     data = json.load(f)
+        #     title, content = random.choice(list(data.items()))
+        #     st.subheader(title)
+        #     for i in range(len(content)):
+        #         st.write(content[i])
 
 if __name__ == '__main__':
     main()
