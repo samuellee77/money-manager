85ab2db5eaaa877f9dab179e8450a07b56d5b070
samuellee77 <hsl023@ucsd.edu>
2023-09-23 13:09:33 -0700

    fix bug

--- src/pages/4_🎲_Random_Generator.py
+++ src/pages/4_🎲_Random_Generator.py
@@ -1,5 +1,6 @@
 import random
 import streamlit as st
+import time
 
 def main():
     st.title("Random Generator")
@@ -11,15 +12,19 @@ def main():
 
     if st.button("Add option"):
         st.session_state.opts.append(opt)
-
     st.write(f"Current Options: [ {' | '.join(st.session_state.opts)} ]")
 
     if st.button("choose!"):
-        st.success(f"The final choice is ... {random.choice(st.session_state.opts)}!")
-
+        if st.session_state.opts:
+            with st.spinner("Please wait..."):
+                time.sleep(1.5)
+            st.success(f"The final choice is ... {random.choice(st.session_state.opts)}!")
+        else:
+            st.error("There are no choices entered! Please enter choices")
+        
     if st.button("clear choices"):
         del st.session_state.opts
-        st.experimental_rerun()
+        st.rerun()
 
 if __name__ == "__main__":
     main()
